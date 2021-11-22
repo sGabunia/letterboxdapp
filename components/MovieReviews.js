f7316a83e1a0aa19ee62692114393968bf4548db
Sergo Gabunia <sergogabunia@gmail.com>
2021-11-22 15:40:00 +0400

    Separated renderItem function from Flatlist

--- components/MovieReviews.js
+++ components/MovieReviews.js
@@ -17,36 +17,35 @@ const MovieReviews = ({movieReviews}) => {
     LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
   }, []);
 
+  const rendetItem = ({item}) => {
+    const {author, content, author_details} = item;
+    return (
+      <View style={styles.reviewsWrapper}>
+        <View style={styles.imageWrapper}>
+          <Image
+            source={{
+              uri: author_details.avatar_path
+                ? `https://secure.gravatar.com/avatar/992eef352126a53d7e141bf9e8707576.jpg`
+                : `https://via.placeholder.com/150`,
+            }}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.contentWrapper}>
+          <AppMainText style={styles.title}>{author}</AppMainText>
+          <Text numberOfLines={10} style={styles.text}>
+            {content}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={{paddingHorizontal: 8}}>
       <AppMainText style={styles.sectionTitle}>REVIEWS</AppMainText>
       {movieReviews.results.length !== 0 ? (
-        <FlatList
-          data={movieReviews.results}
-          renderItem={({item}) => {
-            const {author, content, author_details} = item;
-            return (
-              <View style={styles.reviewsWrapper}>
-                <View style={styles.imageWrapper}>
-                  <Image
-                    source={{
-                      uri: author_details.avatar_path
-                        ? `https://secure.gravatar.com/avatar/992eef352126a53d7e141bf9e8707576.jpg`
-                        : `https://via.placeholder.com/150`,
-                    }}
-                    style={styles.image}
-                  />
-                </View>
-                <View style={styles.contentWrapper}>
-                  <AppMainText style={styles.title}>{author}</AppMainText>
-                  <Text numberOfLines={10} style={styles.text}>
-                    {content}
-                  </Text>
-                </View>
-              </View>
-            );
-          }}
-        />
+        <FlatList data={movieReviews.results} renderItem={rendetItem} />
       ) : (
         <AppMainText style={styles.noReview}>There are no reviews</AppMainText>
       )}
